432379e59dd592086ef0d8b3c002b6587f51960e
Anthony-D99 <anthonydisaf@yahoo.com>
2023-05-09 13:16:37 -0400

    created the function in js to add the search history buttons on search button click and created functionality for them to be research on click.

--- Assets/script.js
+++ Assets/script.js
@@ -9,6 +9,9 @@ var cityName = $(inputSearch).val()
 // step 2 plug in variable name into template literal string
 // var myName = "Allan"
 // var templateLiteralExample = `Hi my name is ${myName}`
+
+
+
 function getWeatherAPI(requestWeatherUrl) {
   fetch(requestWeatherUrl)
     .then(function (response) {
@@ -17,82 +20,125 @@ function getWeatherAPI(requestWeatherUrl) {
     })
     .then(function (data) {
       console.log(data)
+      $('img').remove()
       // current day
       var forecastDate = data.list[0].dt_txt
       var firstDayDate = dayjs(forecastDate).format("(MM/DD/YYYY)")
-      var currentCity = data.city.cityName
-      var currentDayForecastIcon = data.list[0].weather[0].icon
+      var currentCity = data.city.name
       var currentDayForecastTemp = data.list[0].main.temp
       var currentDayForecastWind = data.list[0].wind.speed
       var currentDayForecastHumidity = data.list[0].main.humidity
-      var weatherIconUrl = `https://openweathermap.org/img/wn/${currentDayForecastIcon}@2x.png`
-      cityDate.text(currentCity + " " + firstDayDate + weatherIconUrl)
-      $('#icon1').attr('src', weatherIconUrl)
-      $('#temp1').text(`Temp:${currentDayForecastTemp}°F`)
+
+      function getWeatherIcon() {
+        var currentDayForecastIcon = data.list[0].weather[0].icon
+        var img = $('<img>', {src:`https://openweathermap.org/img/wn/${currentDayForecastIcon}@2x.png`})
+        return img
+      }
+
+
+      cityDate.text(currentCity + " " + firstDayDate)
+      $('#currentDayIcon').append('src', getWeatherIcon(0))
+      $('#temp1').text(`Temp:${currentDayForecastTemp}F`)
       $('#wind1').text(`Wind:${currentDayForecastWind}MPH`)
       $('#humidity1').text(`Humidity:${currentDayForecastHumidity}%`)
+
+
       // first
-      forecastDate = data.list[].dt_txt
+      forecastDate = data.list[1].dt_txt
       firstDayDate = dayjs(forecastDate).format("(MM/DD/YYYY)")
-      var firstDayForecastIcon = data.list[1].weather[9].icon
-      var firstDayForecastTemp = data.list[17].main.temp
-      var firstDayForecastWind = data.list[25].wind.speed
-      var firstDayForecastHumidity = data.list[33].main.humidity
+      var firstDayForecastIcon = data.list[1].weather[0].icon
+      var firstDayForecastTemp = data.list[1].main.temp
+      var firstDayForecastWind = data.list[1].wind.speed
+      var firstDayForecastHumidity = data.list[1].main.humidity
       $('#firstDay').text(firstDayDate)
-      $('#icon2').attr('src', weatherIconUrl)
-      $('#temp2').text(`Temp:${firstDayForecastTemp}°F`)
+      $('#firstDayIcon').append('src', getWeatherIcon(1))
+      $('#temp2').text(`Temp:${firstDayForecastTemp}F`)
       $('#wind2').text(`Wind:${firstDayForecastWind}MPH`)
       $('#humidity2').text(`Humidity:${firstDayForecastHumidity}%`)
+
+
       // second 
-      forecastDate = data.list[].dt_txt
+      forecastDate = data.list[9].dt_txt
       secondDayDate = dayjs(forecastDate).format("(MM/DD/YYYY)")
-      var secondDayForecastIcon = data.list[].weather[].icon
-      var secondDayForecastTemp = data.list[].main.temp
-      var secondDayForecastWind = data.list[].wind.speed
-      var secondDayForecastHumidity = data.list[].main.humidity
+      var secondDayForecastIcon = data.list[9].weather[0].icon
+      var secondDayForecastTemp = data.list[9].main.temp
+      var secondDayForecastWind = data.list[9].wind.speed
+      var secondDayForecastHumidity = data.list[9].main.humidity
       $('#secondDay').text(secondDayDate)
-      $('#icon3').attr('src', weatherIconUrl)
-      $('#temp3').text(`Temp:${secondDayForecastTemp}°F`)
+      $('#secondDayIcon').append('src', getWeatherIcon(9))
+      $('#temp3').text(`Temp:${secondDayForecastTemp}F`)
       $('#wind3').text(`Wind:${secondDayForecastWind}MPH`)
       $('#humidity3').text(`Humidity:${secondDayForecastHumidity}%`)
+
+
       // third
-      forecastDate = data.list[].dt_txt
+      forecastDate = data.list[17].dt_txt
       thirdDayDate = dayjs(forecastDate).format("(MM/DD/YYYY)")
-      var thirdDayForecastIcon = data.list[].weather[].icon
-      var thirdDayForecastTemp = data.list[].main.temp
-      var thirdDayForecastWind = data.list[].wind.speed
-      var thirdDayForecastHumidity = data.list[].main.humidity
+      var thirdDayForecastIcon = data.list[17].weather[0].icon
+      var thirdDayForecastTemp = data.list[17].main.temp
+      var thirdDayForecastWind = data.list[17].wind.speed
+      var thirdDayForecastHumidity = data.list[17].main.humidity
       $('#thirdDay').text(thirdDayDate)
-      $('#icon4').attr('src', weatherIconUrl)
-      $('#temp4').text(`Temp:${thirdDayForecastTemp}°F`)
+      $('#thirdDayIcon').append('src', getWeatherIcon(17))
+      $('#temp4').text(`Temp:${thirdDayForecastTemp}F`)
       $('#wind4').text(`Wind:${thirdDayForecastWind}MPH`)
       $('#humidity4').text(`Humidity:${thirdDayForecastHumidity}%`)
+
+
       // fourth
-      forecastDate = data.list[].dt_txt
+      forecastDate = data.list[25].dt_txt
       fourthDayDate = dayjs(forecastDate).format("(MM/DD/YYYY)")
-      var fourthDayForecastIcon = data.list[].weather[].icon
-      var fourthDayForecastTemp = data.list[].main.temp
-      var fourthDayForecastWind = data.list[].wind.speed
-      var fourthDayForecastHumidity = data.list[].main.humidity
+      var fourthDayForecastIcon = data.list[25].weather[0].icon
+      var fourthDayForecastTemp = data.list[25].main.temp
+      var fourthDayForecastWind = data.list[25].wind.speed
+      var fourthDayForecastHumidity = data.list[25].main.humidity
       $('#fourthDay').text(fourthDayDate)
-      $('#icon5').attr('src', weatherIconUrl)
-      $('#temp5').text(`Temp:${fourthDayForecastTemp}°F`)
+      $('#fourthDayIcon').append('src', getWeatherIcon(25))
+      $('#temp5').text(`Temp:${fourthDayForecastTemp}F`)
       $('#wind5').text(`Wind:${fourthDayForecastWind}MPH`)
       $('#humidity5').text(`Humidity:${fourthDayForecastHumidity}%`)
+
+
       // fith
-      forecastDate = data.list[].dt_txt
+      forecastDate = data.list[33].dt_txt
       fithDayDate = dayjs(forecastDate).format("(MM/DD/YYYY)")
-      var fithDayForecastIcon = data.list[].weather[].icon
-      var fithDayForecastTemp = data.list[].main.temp
-      var fithDayForecastWind = data.list[].wind.speed
-      var fithDayForecastHumidity = data.list[].main.humidity
+      var fithDayForecastIcon = data.list[33].weather[0].icon
+      var fithDayForecastTemp = data.list[33].main.temp
+      var fithDayForecastWind = data.list[33].wind.speed
+      var fithDayForecastHumidity = data.list[33].main.humidity
       $('#fithDay').text(fithDayDate)
-      $('#icon6').attr('src', weatherIconUrl)
-      $('#temp6').text(`Temp:${fithDayForecastTemp}°F`)
+      $('#fithDayIcon').append('src', getWeatherIcon(33))
+      $('#temp6').text(`Temp:${fithDayForecastTemp}F`)
       $('#wind6').text(`Wind:${fithDayForecastWind}MPH`)
       $('#humidity6').text(`Humidity:${fithDayForecastHumidity}%`)
+
+
+      var a=$('.history').text();
+      var b=data.city.name;
+
+      if (a.includes(b)){
+        return
+
+      } else {
+        var button = $("<button>", {class:"history"});
+        button.text(data.city.name);
+        $(searchHistory).prepend(button);
+          if ($("button").length>11)
+        {
+            favorites.find("button:last").remove();
+        }
+      }
+
+
+
+
     })
+
 }
+
+
+
+
 function getApi(requestUrl) {
   fetch(requestUrl)
     .then(function (response) {
@@ -100,28 +146,44 @@ function getApi(requestUrl) {
       return response.json();
     })
     .then(function (data) {
-      console.log(data)
-      var forecastDate = data.list[0].dt_txt
-      var firstDayDate = dayjs(forecastDate).format("(MM/DD/YYYY)")
-      console.log(firstDayDate)
       var lon = data.city.coord.lon
       var lat = data.city.coord.lat
       console.log(lat)
       console.log(lon)
       var requestWeatherUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=3484e08d51e803d19133758ad6e77ac5&units=imperial`;
-      getWeatherApi(requestWeatherUrl)
+      getWeatherAPI(requestWeatherUrl)
 
     })
 
 };
+
+
+
 function userSearch() {
   cityName = $(inputSearch).val()
   localStorage.setItem('city', cityName);
 }
-searchButton.on('click', function () {
+
+
+
+searchButton.on('click', function (event) {
+  event.preventDefault();
   userSearch();
+  $('.displayArea').removeClass('hide')
   var requestGeocodeUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=3484e08d51e803d19133758ad6e77ac5`;
 
-  getApi(requestGeocodeUrl)
+  getApi(requestGeocodeUrl);
+
+
 })
 
+$(searchHistory).on("click",".history", function(){
+  cityName=$(this).text();
+  var requestGeocodeUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=3484e08d51e803d19133758ad6e77ac5`;
+  getApi(requestGeocodeUrl);
+
+  $("#firstDayIcon img").remove();
+
+
+
+})
